38d7f2e8fc040300c243630b3406dbe19356eac6
mtpullin <mtpullin@gmail.com>
2021-05-02 08:54:05 -0400

    added console logs and starting javascript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("Javascript is Running!")
+var playerName = window.prompt("What is your robot's name?");
+//Note the lack of quotation marks around playerName
+console.log(playerName);
+console.log("This logs a streing, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10+10);
+// what is this?
+console.log("Our robot's name is " + playerName); 
+
+function fight() {
+    window.alert("the fight has begun!");
+}
+//fight();
