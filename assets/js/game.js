0fe2f417697d69ff61f004a6cb3a69a3540a2855
mtpullin <mtpullin@gmail.com>
2021-05-03 19:21:59 -0400

    made loop to fight each enemy robot

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,9 @@
+//game states
+//"win"- player robot has defeated all enemy-robots
+// *fight all-enemy-robots
+// *defeat each enemy-robot
+//"lose"- plater robot's health is zero or less
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
@@ -6,11 +12,11 @@ var playerMoney = 10;
 //You can also log multiple calues at once like this//
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = [ "Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -72,4 +78,6 @@ else {
     
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
